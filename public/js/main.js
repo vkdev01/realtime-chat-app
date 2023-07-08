e61ea4c04ddd7b05a9a8da2684f267cd90b47682
vk <vkumar01.dev@gmail.com>
2023-07-08 16:14:39 +0530

    Implemented basic socket.io event handling

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
 
-// Get username and room from URL
+// Get username and room from URL via QS
 const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
 });
@@ -19,9 +19,10 @@ socket.on('roomUsers', ({ room, users }) => {
   outputUsers(users);
 });
 
-// Message from server
+// catching Message from server
 socket.on('message', (message) => {
   console.log(message);
+
   outputMessage(message);
 
   // Scroll down
@@ -32,7 +33,7 @@ socket.on('message', (message) => {
 chatForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  // Get message text
+  // Get message text instead of getting from DOM
   let msg = e.target.elements.msg.value;
 
   msg = msg.trim();
@@ -53,15 +54,20 @@ chatForm.addEventListener('submit', (e) => {
 function outputMessage(message) {
   const div = document.createElement('div');
   div.classList.add('message');
+
   const p = document.createElement('p');
   p.classList.add('meta');
   p.innerText = message.username;
-  p.innerHTML += `<span>${message.time}</span>`;
+  p.innerHTML += `<span> | ${message.time}</span>`;
+  
   div.appendChild(p);
+  
   const para = document.createElement('p');
   para.classList.add('text');
   para.innerText = message.text;
+  
   div.appendChild(para);
+  
   document.querySelector('.chat-messages').appendChild(div);
 }
 
--- server.js
+++ server.js
@@ -1,19 +1,59 @@
 const express = require('express');
 
+const http = require('http');
+
+const socketio = require('socket.io');
+
+
+const formatMessage =  require('./utils/messages');
+
 const path = require('path');
 
 const dotenv = require('dotenv');
+const { Console } = require('console');
 dotenv.config()
 
 // import express from 'express'; // es module syntax
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+const botName = 'ChatBot'
 
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// run when a client connects
+io.on('connection', (socket) => {
+
+    // Welcomes Current user
+    // emits to single user that connecting
+    socket.emit('message', formatMessage(botName, 'Welcome to This Chat Room'));
+
+    // Broadcast when a user connects
+    socket.broadcast.emit('message', formatMessage(botName, 'A new user has joined the chat')); // emits to everybody excepts the user thats connecting
+
+    //Runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', formatMessage(botName, 'A user has left the chat'));
+    });
+
+
+    // Listen for chatMessage
+    socket.on('chatMessage', (msg) =>{
+        console.log(msg);
+
+        io.emit('message', formatMessage('User', msg));
+    });
+});
+
+
+
 const PORT = process.env.PORT || 3000;
 
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
